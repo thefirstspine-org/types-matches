7b60e323bbf211e47bd0e3c494c2f63cae88e7db
TheFirstSpine <teddy@thefirstspine.fr>
2023-10-04 17:30:29 +0530

    Added some fixes

--- src/api-get-users-game-response.interface.ts
+++ src/api-get-users-game-response.interface.ts
@@ -1,9 +1,7 @@
-import { IWizard } from "./wizard.interface";
 import { IGameUser } from "./game-user.interface";
 
 export interface IApiGetUsersResponse {
   users: Array<{
-    account: IWizard,
     game: IGameUser,
   }>;
 }
--- src/game-result.interface.ts
+++ src/game-result.interface.ts
@@ -1,7 +1,4 @@
-import { IWizardItem } from "./wizard-item.interface";
-
 export interface IGameResult {
   user: number;
   result: 'win'|'lose'|'kick'|'concede';
-  loot: IWizardItem[];
 }
