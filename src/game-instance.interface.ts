47c3d185f9252477dc68af2b687b4e871cc66be1
TheFirstSpine <teddy@thefirstspine.fr>
2026-01-31 00:37:46 +0100

    fix(matches): add createdAt property to IGameInstance interface

--- src/game-instance.interface.ts
+++ src/game-instance.interface.ts
@@ -7,6 +7,7 @@ import { IGameResult } from "./game-result.interface";
 
 export interface IGameInstance {
   id: number;
+  createdAt: Date;
   queueKey: string;
   status: 'active'|'ended'|'closed'|'conceded';
   expirationTimeModifier?: number;
